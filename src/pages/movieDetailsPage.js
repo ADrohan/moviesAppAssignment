62b4339c2f1fc249f70eaf5bb1a0ff70fc9c5308
aileendrohan <aileendrohan@gmail.com>
2021-06-12 14:32:47 +0100

    Added routing and live data from API

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
 import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
 import Grid from "@material-ui/core/Grid";
@@ -20,8 +20,40 @@ const useStyles = makeStyles((theme) => ({
 
 const MoviePage = (props) => {
   const classes = useStyles();
-  const movie = props.movie;
-  const images = props.images;
+// remove pssing details as prop
+// const movie = props.movie;
+//  const images = props.images;
+const { id } = props.match.params; // extract id from browser params url address
+const [movie, setMovie] = useState(null);
+const [images, setImages] = useState([]);
+
+// this hook used API movie endpoint to get full details on movie
+useEffect(() => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((movie) => {
+      // console.log(movie)
+      setMovie(movie);
+    });
+}, [id]);
+
+// this hook gets images for movie
+useEffect(() => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.posters)
+    .then((images) => {
+      // console,log(images)
+      setImages(images);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
 
   return (
     <>
@@ -43,8 +75,8 @@ const MoviePage = (props) => {
                       cols={1}
                     >
                       <img
-                        src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image.poster_path}
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.file_path}
                       />
                     </GridListTile>
                   ))}
